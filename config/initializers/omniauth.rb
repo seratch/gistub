0ff707861c99a15faa9da9cf3bdbfce1af8c596d
sue445 <sue445@sue445.net>
2015-07-11 23:59:01 +0900

    If blank, use default value
    because received blank string from app.json

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem'
   provider :open_id,
-           :identifier => ENV['GISTUB_OPENID_IDENTIFIER'] || 'https://www.google.com/accounts/o8/id',
+           :identifier => ENV['GISTUB_OPENID_IDENTIFIER'].presence || 'https://www.google.com/accounts/o8/id',
            :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp/openid")
 end
